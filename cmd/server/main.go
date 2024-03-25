e4f6ae6faffd50fa446fdf8d3d5c365fd8479d4d
Thanh Thien <thien.mlt8601@sinhvien.hoasen.edu.vn>
2024-03-25 17:23:05 +0700

    list + create

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/itss-academy/imago/core/domain/auth"
 	"github.com/itss-academy/imago/core/domain/comment"
 	"github.com/itss-academy/imago/core/domain/profile"
+	"github.com/itss-academy/imago/core/domain/post"
 	authPkgDelivery "github.com/itss-academy/imago/core/internal/auth/delivery"
 	authPkgInterop "github.com/itss-academy/imago/core/internal/auth/interop"
 	authPkgRepo "github.com/itss-academy/imago/core/internal/auth/repo"
@@ -25,6 +26,10 @@ import (
 
 	"log"
 
+	postPkgDelivery "github.com/itss-academy/imago/core/internal/post/delivery"
+	postPkgInterop "github.com/itss-academy/imago/core/internal/post/interop"
+	postPkgRepo "github.com/itss-academy/imago/core/internal/post/repo"
+	postPkgUcase "github.com/itss-academy/imago/core/internal/post/ucase"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/viper"
@@ -76,6 +81,13 @@ func main() {
 	var profileRepo profile.ProfileRepository
 	var profileUsecase profile.ProfileUseCase
 	var profileInterop profile.ProfileInterop
+	var postRepo post.PostRepository
+	var postUsecase post.PostUseCase
+	var postInterop post.PostInterop
+
+	postRepo = postPkgRepo.NewPostRepository(db)
+	postUsecase = postPkgUcase.NewPostUseCase(postRepo)
+	postInterop = postPkgInterop.NewPostBaseInterop(postUsecase, authUsecase)
 
 	authRepo = authPkgRepo.NewAuthRepository(db)
 	authUsecase = authPkgUcase.NewAuthUseCase(authRepo, authClient)
@@ -101,6 +113,9 @@ func main() {
 	authPkgDelivery.NewAuthHttpDelivery(authApi, authInterop)
 	profilePkgDelivery.NewProfileHttpDelivery(profileApi, profileInterop)
 	commentPkgDelivery.NewCommentHttpDelivery(commentApi, commentInterop)
+
+	postApi := e.Group("/v2/post")
+	postPkgDelivery.NewPostHttpDelivery(postApi, postInterop)
 
 	// start server
 	_ = e.Start(fmt.Sprintf("%s:%s", viper.GetString("server.host"), viper.GetString("server.port")))
